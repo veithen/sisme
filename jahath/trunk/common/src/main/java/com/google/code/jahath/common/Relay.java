63a209eee0c3a926c39bb495bc26d24f69efbdc9
andreas.veithen <andreas.veithen@1ab723b5-463c-ed11-3be3-2119154e454a>
2009-10-18 17:20:47 +0000

    Improved logging.

--- jahath/trunk/common/src/main/java/com/google/code/jahath/common/Relay.java
+++ jahath/trunk/common/src/main/java/com/google/code/jahath/common/Relay.java
@@ -25,8 +25,13 @@ public class Relay implements Runnable {
         byte buf[] = new byte[4096];
         try {
             int n;
-            while ((n = in.read(buf)) > 0) {
-                System.out.write(buf, 0, n);
+            while ((n = in.read(buf)) != -1) {
+                if (log.isDebugEnabled()) {
+                    StringBuilder dump = new StringBuilder(tag);
+                    dump.append('\n');
+                    hexDump(dump, buf, n);
+                    log.debug(dump);
+                }
                 out.write(buf, 0, n);
                 out.flush();
             }
@@ -37,5 +42,42 @@ public class Relay implements Runnable {
             IOUtils.closeQuietly(out);
         }
         log.info(tag + " closed");
+    }
+
+    private static void hexDump(StringBuilder buffer, byte[] data, int length) {
+        for (int start = 0; start < length; start += 16) {
+            for (int i=0; i<16; i++) {
+                int index = start+i;
+                if (index < length) {
+                    String hex = Integer.toHexString(data[start+i] & 0xFF);
+                    if (hex.length() < 2) {
+                        buffer.append('0');
+                    }
+                    buffer.append(hex);
+                } else {
+                    buffer.append("  ");
+                }
+                buffer.append(' ');
+                if (i == 7) {
+                    buffer.append(' ');
+                }
+            }
+            buffer.append(" |");
+            for (int i=0; i<16; i++) {
+                int index = start+i;
+                if (index < length) {
+                    int b = data[index] & 0xFF;
+                    if (32 <= b && b < 128) {
+                        buffer.append((char)b);
+                    } else {
+                        buffer.append('.');
+                    }
+                } else {
+                    buffer.append(' ');
+                }
+            }
+            buffer.append('|');
+            buffer.append('\n');
+        }
     }
 }
