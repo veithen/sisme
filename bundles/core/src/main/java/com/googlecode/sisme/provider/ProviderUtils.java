667f098b3907ded114d2177e8e4800b806414b20
Andreas Veithen <andreas.veithen@gmail.com>
2011-02-20 16:06:58 +0000

    Some more experimental code.

---
+++ bundles/core/src/main/java/com/googlecode/sisme/provider/ProviderUtils.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2011 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.sisme.provider;
+
+import java.util.Properties;
+
+import javax.xml.namespace.QName;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public final class ProviderUtils {
+    private ProviderUtils() {}
+    
+    public static ServiceRegistration registerManagedObjectFactory(BundleContext context, ManagedObjectFactory factory, QName name) {
+        Properties props = new Properties();
+        props.put("namespace", name.getNamespaceURI());
+        props.put("name", name.getLocalPart());
+        return context.registerService(ManagedObjectFactory.class.getName(), factory, props);
+    }
+}
