352c8ca86ca28f4307514e482212350c5fc48c89
andreas.veithen <andreas.veithen@1ab723b5-463c-ed11-3be3-2119154e454a>
2011-02-23 16:58:07 +0000

    Some more experimental code.

---
+++ trunk/bundles/framework/src/main/java/com/googlecode/sisme/framework/parser/ManagedObject.java
@@ -0,0 +1,30 @@
+package com.googlecode.sisme.framework.parser;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.ServiceRegistration;
+
+class ManagedObject {
+    private final Binder binder;
+    private final String[] clazzes;
+    private final ManagedObjectFactory factory;
+    private final Dictionary properties;
+    private ServiceRegistration registration;
+
+    public ManagedObject(Binder binder, String[] clazzes,
+            ManagedObjectFactory factory, Dictionary properties) {
+        this.binder = binder;
+        this.clazzes = clazzes;
+        this.factory = factory;
+        this.properties = properties;
+    }
+
+    void register() {
+        Object service = factory.createObject();
+        registration = binder.getBundleContext().registerService(clazzes, service, properties);
+    }
+    
+    void unregister() {
+        registration.unregister();
+    }
+}
